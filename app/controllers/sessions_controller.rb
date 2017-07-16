4ce97375e1125b8b8d3c984b6e398a72e45134b0
Nicola Ng <ngnicola513@gmail.com>
2017-07-16 18:21:39 +0800

    error message change to red color and check user exists

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,24 @@
 class SessionsController < ApplicationController
    def create
-    user = User.from_omniauth(env["omniauth.auth"])
+    if User.exists?(:uid=> env["omniauth.auth"].uid)
+     session[:exists] = true
+     user = User.find_by_uid(env["omniauth.auth"].uid)
+    else
+     session[:exists] = false
+     user = User.from_omniauth(env["omniauth.auth"])
+    end
+    
     session[:user_id] = user.id
     session[:user_name] = user.name
-    redirect_to session[:return_to]
+    if session[:return_to]!=nil
+     redirect_to session[:return_to]
+    else
+     redirect_to '/articles'
+    end
   end
 
   def destroy
+    session[:exists] = nil
     session[:user_id] = nil
     session[:user_name] = nil
     redirect_to root_path
